bad9ce3cd77e1cf9d0cd95db5362dec8d7da4747
McCormack Leo <leo.t.mccormack@gmail.com>
2018-07-19 12:28:02 +0300

    updated uniform_sph.h

--- test/uniform_sph.h
+++ test/uniform_sph.h
@@ -1,6 +1,20 @@
 #ifndef UNIFORM_SPH_H
 #define UNIFORM_SPH_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+    
+extern const float __Tdesign_degree_7_dirs_deg[24][2];
+extern const float __Tdesign_degree_9_dirs_deg[48][2];
+extern const float __Tdesign_degree_18_dirs_deg[180][2];
+extern const float __Tdesign_degree_40_dirs_deg[840][2];
+extern const float __Tdesign_degree_100_dirs_deg[5100][2];
+    
+#ifdef __cplusplus
+} /*extern "C"*/
+#endif
+
 /* References:
  [1] McLaren's Improved Snub Cube and Other New Spherical Designs in Three Dimensions", R. H. Hardin and
  N. J. A. Sloane, Discrete and Computational Geometry, 15 (1996), pp. 429-441.
